79559ef56d60c204a9bc5255a7cb0195b910d237
dvcorreia <dv.correia@ua.pt>
2018-06-11 00:50:19 +0100

    load file bug fixed

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,36 +1,53 @@
 import React, { Component, Fragment } from 'react'
-import Graph from './Graph.jsx'
 import Search from './Search.jsx'
 import DataShow from './DataShow.jsx'
 
-//import dataDummy from './Graphs/data.json'
+import SmithChart from './Graphs/SmithChart.jsx'
+import LogMag from './Graphs/LogMag.jsx'
+
+const graphContainer = {
+    width: "100%", 
+    height: 260
+}
 
 export default class Content extends Component{
+    state = {
+        color: "#3273dc"
+    }
+
     render(){
         return(
             <Fragment>
                 <div id="content" className="columns">
-                    <div className="column is-three-quarters" style={{
-                        padding: 15
-                    }}>
-                        <Graph 
-                            data={this.props.data.p21} 
-                            graphType='polar'/>
-                        <Graph 
-                            data={this.props.data.p11} 
-                            graphType='logMag'/>
+                    <div className="column is-three-quarters" style={{ padding: 15 }}>
+
+                        <div style={graphContainer}>
+                            <LogMag 
+                                data={this.props.data.p11} 
+                                color={this.state.color}/>
+                        </div>
+
+                        <div style={graphContainer}>
+                            <LogMag 
+                                data={this.props.data.p11} 
+                                color={this.state.color}/>
+                        </div>
+
                     </div>
-                    <div className="column" style={{
-                        padding: 25
-                    }}>
+                    <div className="column" style={{ padding: 25 }}>
+
                         <Search onChangeBW={this.props.onChangeBW}/>
+
                         <hr/>
+
                         <DataShow data={this.props.data}/>
+
                     </div>
                 </div>
+
                 <div id="content" className="columns is-centered">
                     <div className="column is-three-quarters">
-                        <Graph data={this.props.data.p11} graphType='smith'/>
+                        <SmithChart />
                     </div>
                 </div>
             </Fragment>
--- src/components/Content/Graphs/LogMag.jsx
+++ src/components/Content/Graphs/LogMag.jsx
@@ -3,58 +3,63 @@ import { Scatter } from 'react-chartjs-2';
 
 export default class LogMag extends Component{
     state = {
-        data: this.props.data
-    }
-
-    chartData = {
-        datasets:[
-            {
-                label: "S11 LogMag Graph",
-                fill: false,
-                backgroundColor: this.props.color,
-                pointBorderColor: this.props.color,
-                pointBackgroundColor: '#ffffff',
-                pointBorderWidth: 1,
-                pointHoverRadius: 1,
-                pointRadius: 0.25,
-                pointHitRadius: 2,
-                data: this.state.data,
+        chartData: {
+            datasets:[
+                {
+                    label: "S11 LogMag Graph",
+                    fill: false,
+                    backgroundColor: this.props.color,
+                    pointBorderColor: this.props.color,
+                    pointBackgroundColor: '#ffffff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 1,
+                    pointRadius: 0.25,
+                    pointHitRadius: 2,
+                    data: this.props.data,
+                }
+            ]
+        },
+        chartOptions: {
+            maintainAspectRatio: false,
+            showLine: true,
+            scales: {
+                xAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Real"
+                    }
+                }],
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Imaginary"
+                    }
+                }]
             }
-        ]
+        } 
     }
 
-    chartOptions = {
-        maintainAspectRatio: false,
-        showLine: true,
-        scales: {
-            xAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: "Real"
-                },
-                ticks: {
-                    suggestedMin: -1.5,
-                    suggestedMax: 1.5
-                }
-            }],
-            yAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: "Imaginary"
-                },
-                ticks: {
-                    suggestedMin: -1.5,
-                    suggestedMax: 1.5
-                }
-            }]
+    componentDidUpdate(prevProps, prevState, snapshot){
+        if(prevState.chartData.datasets[0].data !== this.props.data){
+            this.setState(
+                Object.assign({}, prevState, {
+                    chartData: {
+                        datasets:[
+                            Object.assign({}, prevState.chartData.datasets[0],{
+                                data: this.props.data
+                            })
+                        ]
+                    }
+                })
+            )
         }
     }
     
     render(){
         return(
                 <Scatter
-                    data={this.chartData}
-                    options={this.chartOptions}
+                    data={this.state.chartData}
+                    options={this.state.chartOptions}
                     style={{position: 'absolute'}}
                 />
         )
